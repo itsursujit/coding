d414535016081b9f0d29bc0b2951c2b3b064027e
Sujit Baniya <itsursujit@gmail.com>
2019-06-09 17:33:43 +0545

    Initial Commit

--- config/database.php
+++ config/database.php
@@ -85,6 +85,12 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
     ],
 
     /*
